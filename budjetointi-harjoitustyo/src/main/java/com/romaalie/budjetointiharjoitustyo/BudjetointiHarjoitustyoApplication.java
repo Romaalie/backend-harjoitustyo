b6fdfcbae529957fc3661e0bdee640acdd0d73f8
Ali Romar <ali.romar@myy.haaga-helia.fi>
2024-03-16 10:34:34 +0200

    Luotu Spring Boot projekti

---
+++ budjetointi-harjoitustyo/src/main/java/com/romaalie/budjetointiharjoitustyo/BudjetointiHarjoitustyoApplication.java
@@ -0,0 +1,13 @@
+package com.romaalie.budjetointiharjoitustyo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudjetointiHarjoitustyoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudjetointiHarjoitustyoApplication.class, args);
+	}
+
+}
---
+++ budjetointi-harjoitustyo/src/test/java/com/romaalie/budjetointiharjoitustyo/BudjetointiHarjoitustyoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.romaalie.budjetointiharjoitustyo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudjetointiHarjoitustyoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
