5f7a3ea236f6e6add31e5e3f91964148cbc42213
Ali Romar <ali.romar@myy.haaga-helia.fi>
2024-03-22 18:29:06 +0200

    MenoeraRepoTests 2kpl update testeja.

--- budjetointi-harjoitustyo/src/test/java/com/romaalie/budjetointiharjoitustyo/MenoeraRepoTests.java
+++ budjetointi-harjoitustyo/src/test/java/com/romaalie/budjetointiharjoitustyo/MenoeraRepoTests.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -43,11 +44,12 @@ class MenoeraRepoTests {
     @Autowired
     KayttajaRepository kayttajaRepository;
     private Kayttaja maksaja;
+    private Kayttaja maksaja2;
 
     @Autowired
     AliluokkaRepository aliluokkaRepository;
     private Aliluokka aliluokka;
-
+    private Aliluokka aliluokka2;
 
     @BeforeEach
     public void setUp() {
@@ -62,16 +64,22 @@ class MenoeraRepoTests {
         paaluokka = new Paaluokka("Lapset");
         paaluokkaRepository.save(paaluokka);
 
-        //Luodaan uusi käyttäjä ja tallennetaan se tietokantaan.
+        //Luodaan kaksi käyttäjää ja tallennetaan ne tietokantaan.
         maksaja = new Kayttaja("testikayttaja", "$2y$10$q2AlKdGpIEmqCGYgGyYdYeuPbChJZ0FOGYw7wlBT1GcdcYPIJQyUi", "kayttaja");
+        maksaja2 = new Kayttaja("testikayttaja2", "$2y$10$TTntZIC/l1Bw2gIZ1LDtP.ITwpoPgfUuvql0.brvHnEzjFFDQGVJ2", "kayttaja");
         kayttajaRepository.save(maksaja);
+        kayttajaRepository.save(maksaja2);
 
-        //Luodaan uusi aliluokka ja tallennetaan se tietokantaan.
+        //Luodaan kaksi uutta aliluokkaa ja tallennetaan ne tietokantaan.
         aliluokka = new Aliluokka("Lelut", paaluokka);
         aliluokkaRepository.save(aliluokka);
         paaluokka.addAliluokka(aliluokka);
         paaluokkaRepository.save(paaluokka);
 
+        aliluokka2 = new Aliluokka("Vaatteet", paaluokka);
+        aliluokkaRepository.save(aliluokka2);
+        paaluokka.addAliluokka(aliluokka2);
+        paaluokkaRepository.save(paaluokka);
 
     }
 
@@ -84,12 +92,50 @@ class MenoeraRepoTests {
         menoeraRepository.save(menoera);
 
         //Haetaan menoerä tietokannasta. Näin varmistetaan, ettei käytetä välimuistin tietoja.
-        
         Optional<Menoera> haettuMenoera = menoeraRepository.findById(menoera.getId());
-        
+
         assertTrue(haettuMenoera.isPresent());
         assertEquals("Uusi nuija nukutukseen", haettuMenoera.get().getLisatietoja());
         assertEquals("Lelut", haettuMenoera.get().getAliluokka().getNimi());
+
+    }
+
+    //Testaa tiedon päivittämistä tietokantaan.
+    @Test
+    public void menoeraMuutosTesti() {
+
+        //Luodaan uusi menoerä ja tallennetaan se tietokantaan.
+        Menoera menoera = new Menoera(10.00, LocalDate.parse("2024-03-03"), "Uusi nuija nukutukseen", maksaja, paaluokka, aliluokka);
+        menoeraRepository.save(menoera);
+
+        //Muutetaan menoerän nimeä ja tallennetaan muutos tietokantaan.
+        menoera.setMaksaja(maksaja2);
+        menoeraRepository.save(menoera);
+
+        //Haetaan menoerä tietokannasta. Näin varmistetaan, ettei käytetä välimuistin tietoja.
+        Optional<Menoera> haettuMenoera = menoeraRepository.findById(menoera.getId());
+        assertEquals("testikayttaja2", haettuMenoera.get().getMaksaja().getNimi());
+        assertNotEquals("testikayttaja", haettuMenoera.get().getMaksaja().getNimi());
+    }
+
+    //Testaa aliluokan muuttamista menoerässä ja tietojen päivittymistä kaikissa siihen liittyvissä kentissä.
+    @Test
+    public void menoeraAliluokkaMuutosTesti() {
+
+        //Luodaan uusi menoerä ja tallennetaan se tietokantaan.
+        Menoera menoera = new Menoera(10.00, LocalDate.parse("2024-03-03"), "Uusi nuija nukutukseen", maksaja, paaluokka, aliluokka);
+        menoeraRepository.save(menoera);
+
+        //Muutetaan menoerän aliluokkaa.
+        menoera.setAliluokka(aliluokka2);
+        menoeraRepository.save(menoera);
+
+        //Haetaan menoerä tietokannasta. Näin varmistetaan, ettei käytetä välimuistin tietoja.
+        Optional<Menoera> haettuMenoera = menoeraRepository.findById(menoera.getId());
+
+        //Testataan että myös aliluokkaNimi kenttä on päivittynyt.
+        assertEquals("Vaatteet", haettuMenoera.get().getAliluokkaNimi());
+        assertNotEquals("Lelut", haettuMenoera.get().getAliluokkaNimi());
 
     }
 
@@ -109,9 +155,6 @@ class MenoeraRepoTests {
 
         //Tarkistetaan, että menoerä on poistettu.
         assertFalse(haettuMenoera.isPresent());
-
     }
-
-    //TÄÄLTÄ PUUTTUU UPDATE TESTI??
 
 }
